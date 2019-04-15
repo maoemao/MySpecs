ba9a798f84c92f033c8a197e12ae15c49fdb4237
zoujiamao <zoujiamao@aiyaapp.com>
2019-04-15 14:37:38 +0800

    1

---
+++ Classes/Person.h
@@ -0,0 +1,17 @@
+//
+//  Person.h
+//  OCDemo
+//
+//  Created by edz on 2019/4/15.
+//  Copyright © 2019年 edz. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Person : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Classes/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  OCDemo
+//
+//  Created by edz on 2019/4/15.
+//  Copyright © 2019年 edz. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
